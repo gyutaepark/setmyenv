acd9bdc5feadc9379e6be5f88071d89913e8788d
gyutaepark <gtpark96@gmail.com>
2017-12-27 13:02:07 +0900

    Streamlined Code

--- startup.sh
+++ startup.sh
@@ -1,72 +1,133 @@
 conda_url='https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh'
-download_dir='$HOME/SetMyEnvironment/Anaconda.sh'
 cyclus_deps='sudo apt-get install -y cmake make libboost-all-dev libxml2-dev libxml++2.6-dev \
 libsqlite3-dev libhdf5-serial-dev libbz2-dev coinor-libcbc-dev coinor-libcoinutils-dev \
 coinor-libosi-dev coinor-libclp-dev coinor-libcgl-dev libblas-dev liblapack-dev g++ \
 libgoogle-perftools-dev python3-dev python3-tables python3-pandas python3-numpy python3-nose \
 python3-jinja2 cython3'
 
-echo "What Do I do?"
-select yn in "Bash" "+Anaconda" "+Misc" "+CyclusDeps" "+GitBranches"; do
-    case $yn in
-        Bash ) cat toBash.txt >> $HOME/.bashrc; 
-			source $HOME/.bashrc; 
-			break;;
-        +Anaconda ) cat toBash.txt >> $HOME/.bashrc; 
-			source $HOME/.bashrc;
-			curl $conda_url -o $download_dir
-			bash Anaconda.sh; 
-			rm Anaconda.sh; 
-			break;;
-		+Misc ) cat toBash.txt >> $HOME/.bashrc; 
-			source $HOME/.bashrc;
-			curl $conda_url -o $download_dir
-			bash Anaconda.sh; 
-			rm Anaconda.sh; 
-			sudo add-apt-repository -y ppa:webupd8team/sublime-text-3;
-			sudo apt-get update;
-			sudo apt install -y sublime-text-installer git gnome-terminal;
-			break;;
-		+CyclusDeps ) cat toBash.txt >> $HOME/.bashrc; 
-			source $HOME/.bashrc;
-			curl $conda_url -o $download_dir
-			bash Anaconda.sh; 
-			rm Anaconda.sh; 
-			sudo add-apt-repository -y ppa:webupd8team/sublime-text-3;
-			sudo apt-get update;
-			sudo apt install -y sublime-text-installer git gnome-terminal;
-			eval $cyclus_deps;
-			sudo apt-get -y dist-upgrade;
-			break;;
-		+GitBranches ) cat toBash.txt >> $HOME/.bashrc; 
-			source $HOME/.bashrc;
-			curl $conda_url -o $download_dir
-			bash Anaconda.sh; 
-			rm Anaconda.sh; 
-			sudo add-apt-repository -y ppa:webupd8team/sublime-text-3;
-			sudo apt-get update;
-			sudo apt install -y sublime-text-installer git gnome-terminal;
-			eval $cyclus_deps;
-			sudo apt-get -y dist-upgrade;
-			echo "Enter Git User Email";
-			read email;
-			ssh-keygen -t rsa -b 4096 -C $email;
-			echo "Copy and Paste this for your ssh"
-			echo "$(cat $HOME/.ssh/id_rsa.pub)"
-			git config --global user.email $email;
-			echo "Enter Git User Name";
-			read name;
-			git config --global user.name $name;
-			echo "How many branches do you need to clone?"
-			read count;
-			if [ $count -gt 0]
-				then
-					for i in $count; do
-						echo "Link?";
-						read branch;
-						git clone $branch;
-					done
-			fi
-			break;;
-    esac
+echo "Update Bashrc? (Append conda path, parse git branch, alias, etc.)"
+select bashrc in "Yes" "No"; do
+	case $bashrc in
+		Yes ) break;;
+		No )  break;;
+esac
 done
+
+echo "Install Conda 3? (5.0.1)?"
+select conda in "Yes" "No"; do
+	case $conda in
+		Yes ) break;;
+		No )  break;;
+esac
+done
+
+echo "Setup Git?"
+select github in "Yes" "No"; do
+	case $github in
+		Yes ) break;;
+		No )  break;;
+esac
+done
+
+echo "Install Cyclus Dependencies?"
+select cyclus in "Yes" "No"; do
+	case $cyclus in
+		Yes ) break;;
+		No )  break;;
+esac
+done
+
+echo "Install Sublime Text?"
+select sublime in "Yes" "No"; do
+	case $sublime in
+		Yes ) break;;
+		No )  break;;
+esac
+done
+
+echo "Install Other Software?"
+select other in "Yes" "No"; do
+	case $other in
+		Yes ) break;;
+		No )  break;;
+esac
+done
+
+if [[ $bashrc == "Yes" ]]; then
+	cat toBash.txt >> $HOME/.bashrc;
+	source $HOME/.bashrc;
+fi
+
+if [[ $conda == "Yes" ]]; then
+	curl $conda_url -o Anaconda.sh
+	bash Anaconda.sh -b -p $HOME/anaconda3
+	rm Anaconda.sh
+	source $HOME/.bashrc
+	conda config --add -y channels conda-forge
+	conda update -all -y
+	echo "Install Cyclus Dependencies?"
+	select condacyclus in "Yes" "No"; do
+		case $condacyclus in
+			Yes ) conda install cyclus-build-deps -y
+				break;;
+			No ) break;;
+		esac
+	done
+	echo "Install Pyne?"
+	select condapyne in "Yes" "No"; do
+		case $condapyne in
+			Yes ) conda install pyne -y
+				break;;
+			No ) break;;
+		esac
+	done
+	echo "Install Jupyter Extensions?"
+	select nbextension in "Yes" "No"; do
+		case $nbextension in
+			Yes ) conda instal -y jupyter_contrib_nbextensions 
+				break;;
+			No ) break;;
+		esac
+	done
+fi
+
+if [[ $github == "Yes" ]]; then
+	echo "Enter Git User Email";
+	read email;
+	ssh-keygen -t rsa -b 4096 -C $email;
+	echo "Copy and Paste this for your ssh"
+	echo "$(cat $HOME/.ssh/id_rsa.pub)"
+	git config --global user.email $email;
+	echo "Enter Git User Name";
+	read name;
+	git config --global user.name $name;
+	echo "How many branches do you need to clone?"
+	read count;
+	if [[ $count -gt 0 ]]; then
+		for i in $count; do
+			echo "Link?";
+			read branch;
+			git clone $branch;
+		done
+	fi
+fi
+
+if [[ $sublime == "Yes" ]]; then
+	sudo add-apt-repository -y ppa:webupd8team/sublime-text-3;
+	sudo apt-get update;
+	sudo apt-get dist-upgrade -y
+	sudo apt-get install -y sublime-text-installer
+fi
+
+if [[ $cyclus == "Yes" ]]; then
+	sudo apt-get update;
+	sudo apt-get dist-upgrade -y
+	eval cyclus_deps;
+fi
+
+if [[ $other == "Yes" ]]; then
+	sudo apt-get update;
+	echo "List all apt to get"
+	read aptget;
+	sudo apt-get install -y $aptget
+fi
