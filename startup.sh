d01ba4fb562069657bd274d88f282e2b4a2fd33c
gyutaepark <gyutaepark96+github@gmail.com>
2020-03-20 18:12:11 +0900

    Fixed accidental comment

--- startup.sh
+++ startup.sh
@@ -159,14 +159,14 @@ sudo apt update;
 sudo apt install -y $aptlist;
 
 if [[ $conda == "Yes" ]]; then
-	# wget -O - https://www.anaconda.com/distribution/ 2>/dev/null \
-	# | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p' \
-	# | xargs wget -O Anaconda3.sh
-	# bash Anaconda3.sh -b -p $HOME/anaconda3
-	# rm Anaconda3.sh
+	wget -O - https://www.anaconda.com/distribution/ 2>/dev/null \
+	| sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p' \
+	| xargs wget -O Anaconda3.sh
+	bash Anaconda3.sh -b -p $HOME/anaconda3
+	rm Anaconda3.sh
 	echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> $HOME/.bashrc
 	export PATH="$HOME/anaconda3/bin:$PATH"
-	# conda config --add channels conda-forge
+	conda config --add channels conda-forge
 fi
 
 if [[ $condacyclus == "Yes" ]]; then
